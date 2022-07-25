c5d433919f583f8afad5c9b464a49801854c5fc5
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-25 15:10:13 +0200

    init project

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const day = new Vue(
+    {
+        el: '#day',
+        data:{
+            list:[
+                {
+                    text: 'Svegliarsi presto',
+                    done: false
+                },
+                
+                {
+                    text: 'Studiare con Boolean',
+                    done: true
+                },
+                {
+                    text: 'Fare pranzo',
+                    done: false
+                },
+                
+                {
+                    text: 'Disperare tutto il pomeriggio',
+                    done: true
+                },
+                {
+                    text: 'Fare cena',
+                    done: false
+                },
+                
+                {
+                    text: 'Disperare ancora',
+                    done: true
+                },
+            ]
+        }
+    }
+    
+    
+);
